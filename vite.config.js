905290e232071dce94c12376a600924d949a8131
SaickVectrum <129540818+SaickVectrum@users.noreply.github.com>
2025-01-20 09:47:23 -0500

    Install and config Laravel UI

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
+	//Está parte se suele usar para docker
+	server: {
+		host: '0.0.0.0',
+		port: 5173,
+		hmr: {
+			host: '127.0.0.1'
+		}
+	},
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
-        }),
-    ],
+		}),
+		//Acá se le indica que va a trabajar con vue
+        vue(),
+	],
+	//Esto muestra advertencias cuando un chunk sobrepase los 1600 KB
+	build: { chunkSizeWarningLimit: 1600},
+    resolve: {
+        alias: {
+			vue: 'vue/dist/vue.esm-bundler.js',
+			'@': path.resolve(__dirname, 'resources/js'),
+			'~': path.resolve(__dirname, 'node_modules')
+        },
+    },
 });
